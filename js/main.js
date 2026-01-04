c62db92d23938f8fe53ebd20944bebb64148dea1
Gutierrez3003 <gutierrezandrade3003@outlook.com>
2026-01-04 00:09:16 -0300

    docs: adiciona fluxograma e ajustes no kanban

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log("Meta 2026 iniciado");
+const tasks = [
+    {
+        id:1,
+        tiutlo: "Definir metas 2026",
+        status: "a fazer"
+    },
+    {
+        id: 2,
+        titulo: "Criar estrutura do projeto",
+        status: "andamento"
+    },
+    {
+        id: 3,
+        titulo: "Iniciar repositório no GitHub",
+        status: "concluido"
+    }
+];
+
+function renderTasks() {
+    const columns = document.querySelectorAll(".columns");
+
+    columns.forEach(column => {
+        column.querySelectorAll(".card").forEach(card=> card.remove());
+
+        const status= column.dataset.status;
+
+        tasks
+            .filter(tasks => tasks.status === status)
+            .forEach(task => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+                card.dataset.taskId = task.id;
+                card.innerHTML = '<p>$(task.titulo)</p>';
+                column.appendChild(card);
+
+            })
+    })
+}
+
+document.addEventListener("DOMContentLoaded", renderTasks);
